e5a4ac93a9b99b79f35fa0fbc372542478f82655
Amel <amel.kamtapangui@antreem.com>
2023-03-12 20:21:15 +0100

    third day

--- main.kt
+++ main.kt
@@ -71,6 +71,132 @@ class Play(private val me:MySharp, private val opponent: OpponentSharp ) {
     
 }
 
+val lowerCaseAlphabeticMap = mapOf(
+                "a" to 1, 
+                "b" to 2,
+                "c" to 3,
+                "d" to 4,
+                "e" to 5, 
+                "f" to 6,
+                "g" to 7,
+                "h" to 8,
+                "i" to 9, 
+                "j" to 10,
+                "k" to 11,
+                "l" to 12,
+                "m" to 13, 
+                "n" to 14,
+                "o" to 15,
+                "p" to 16,
+                "q" to 17, 
+                "r" to 18,
+                "s" to 19,
+                "t" to 20,
+                "u" to 21, 
+                "v" to 22,
+                "w" to 23,
+                "x" to 24,
+               "y" to 25,
+               "z" to 26,
+                
+               )
+
+val upperCaseAlphabeticMap = mapOf(
+                "a" to 27, 
+                "b" to 28,
+                "c" to 29,
+                "d" to 30,
+                "e" to 31, 
+                "f" to 32,
+                "g" to 33,
+                "h" to 34,
+                "i" to 35, 
+                "j" to 36,
+                "k" to 37,
+                "l" to 38,
+                "m" to 39, 
+                "n" to 40,
+                "o" to 41,
+                "p" to 42,
+                "q" to 43, 
+                "r" to 44,
+                "s" to 45,
+                "t" to 46,
+                "u" to 47, 
+                "v" to 48,
+                "w" to 49,
+                "x" to 50,
+                 "y" to 51,
+                "z" to 52,
+                
+               )
+
+
+class CharacterToArrange(private val contentRucksacks: Array<String>) {
+    
+     private  fun getFirstPartOfString( stringCharatere: String): String{
+        return stringCharatere.substring(0, stringCharatere.length/2)
+        }
+    
+    private  fun getScondPartOfString(stringCharatere: String): String {
+        
+        return stringCharatere.substring(stringCharatere.length/2, stringCharatere.length)
+        
+       }
+    
+    
+    private fun findSimilareElement( element: String): Int{
+        val firstElement = getFirstPartOfString(element)
+        
+        val secondCharacter = getScondPartOfString(element)
+        
+        
+        var priority:Int = 0
+        
+        
+        firstElement.forEach({ 
+       if( secondCharacter.contains(it)) {
+          
+          priority = gerPriority(it.toString())
+       
+       }
+    })
+        
+        return priority
+        
+    }
+    
+      
+  
+    
+private fun gerPriority(letter: String): Int {
+    
+    if(letter.toCharArray()[0].isUpperCase()) {
+      
+        return upperCaseAlphabeticMap[letter.lowercase()]?: 0
+    } else {
+      
+        return lowerCaseAlphabeticMap[letter]?: 0
+    }
+   
+}
+
+ fun getPrioritySum(): Int{
+       
+       var sumV = 0
+       
+       contentRucksacks.forEach({
+          sumV = sumV + findSimilareElement(it)
+       })
+       
+      // println("sumV $sumV")
+       
+       return sumV
+   }
+     
+    
+}
+
 
 
 fun main() {
@@ -81,6 +207,8 @@ fun main() {
 
     
     println(CountCalorie(items).maxNumber)
+
+     
 
     
     var opponentScor = 0
@@ -145,4 +273,14 @@ fun main() {
     
    
     println(finalWinner(opponentScor, myScor))
+    
+     println( CharacterToArrange(arrayOf(
+       "vJrwpWtwJgWrhcsFMMfFFhFp",
+       "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+       "PmmdzqPrVvPwwTWBwg",
+      "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+      "ttgJtRGJQctTZtZT",
+     "CrZsJsPPZsGzwwsLwLmpwMDw"
+   
+   )).getPrioritySum())
 }
